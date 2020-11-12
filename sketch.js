9a6fb74c5cb5d3ff25bb2bf8e9db5bfdac36a8bd
irinsajan <69659076+irinsajan@users.noreply.github.com>
2020-11-12 16:33:10 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,10 +7,7 @@ var bob1, bob2, bob3, bob4, bob5;
 var roof;
 var rope1, rope2, rope3, rope4, rope5;
 
-function preload()
-{
-	
-}
+
 
 function setup() {
 	createCanvas(800, 700);
@@ -26,18 +23,18 @@ function setup() {
   	bob4 = new bob(495,600,50);
   	bob5 = new bob(595,600,50);
 
-	rope1 = new Rope(bob1.body, roof.body, -100, 0);
-	rope2 = new Rope(bob2.body, roof.body, -200, 0);
-	rope3 = new Rope(bob3.body, roof.body, -300, 0);
-	rope4 = new Rope(bob4.body, roof.body, -400, 0);
-	rope5 = new Rope(bob5.body, roof.body, -500, 0);
+	rope1 = new Rope(bob1.body, roof.body, -200, 0);
+	rope2 = new Rope(bob2.body, roof.body, -100, 0);
+	rope3 = new Rope(bob3.body, roof.body, 0, 0);
+	rope4 = new Rope(bob4.body, roof.body, 100, 0);
+	rope5 = new Rope(bob5.body, roof.body, 200, 0);
 	
 	Engine.run(engine);
 }
 
 
 function draw() {
-  rectMode(CENTER);
+  
   background(255, 102, 255);
 
   bob1.display();
@@ -54,7 +51,7 @@ function draw() {
   rope4.display();
   rope5.display();
   
-  drawSprites();
+
 }
 
 
